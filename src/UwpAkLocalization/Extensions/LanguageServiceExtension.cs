c9e3209c4aa2eb5b5bad7a0efab6721b45fc4b58
Michael Gerfen <gerfen@hotmail.com>
2021-02-07 01:16:36 -0800

    Added a UWP sample app which demonstrates localization.

---
+++ src/UwpAkLocalization/Extensions/LanguageServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AKSoftware.Localization.MultiLanguages;
+
+namespace UwpAkLocalization.Extensions
+{
+    public static class LanguageServiceExtension
+    {
+        public static string GetValue(ILanguageContainerService localization, string key)
+        {
+            return localization[key];
+        }
+    }
+}
---
+++ src/UwpAkLocalization/LocalizationKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpAkLocalization
+{
+    public static class LocalizationKeys
+    {
+        public const string HomePage_HelloWorld = "HomePage:HelloWorld";
+    }
+}
