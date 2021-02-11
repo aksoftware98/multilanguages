fe95adfb3b733570b3330aa0f20f633f78ca3c18
Michael Gerfen <gerfen@hotmail.com>
2021-02-10 18:50:23 -0800

    Added LocalizeStringExtension.

---
+++ src/AKSoftware.Localization.MultiLanguages.UWP/Extensions/LocalizeStringExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Markup;
+
+namespace AKSoftware.Localization.MultiLanguages.UWP.Extensions
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public class LocalizeStringExtension : MarkupExtension
+    {
+        public string Key { get; set; }
+
+        public bool Capitalize { get; set; }
+
+        protected override object ProvideValue()
+        {
+            var language = ((IServiceProviderHost)Windows.UI.Xaml.Application.Current).ServiceProvider.GetService<ILanguageContainerService>();
+            var value = language?[Key];
+
+            return Capitalize ? value?.ToUpperInvariant() : value;
+
+        }
+    }
+}
