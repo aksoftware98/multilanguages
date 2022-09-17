c2a88bed6aa2ef8508ffe99730367ae332238235
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-09-17 11:06:22 +0400

    Register the language container as scoped for Blazor Server

--- src/AKSoftware.Localization.MultiLanguages/ServiceExtensions.cs
+++ src/AKSoftware.Localization.MultiLanguages/ServiceExtensions.cs
@@ -18,7 +18,7 @@ namespace AKSoftware.Localization.MultiLanguages
         {
             services.AddSingleton<IKeysProvider, EmbeddedResourceKeysProvider>(s =>
                 new EmbeddedResourceKeysProvider(assembly, folderName));
-            return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            return services.AddSingleton<ILanguageContainerService, LanguageContainerInAssembly>(s =>
             {
                 var keysProvider = s.GetService<IKeysProvider>();
                 return new LanguageContainerInAssembly( culture, keysProvider);
@@ -34,7 +34,7 @@ namespace AKSoftware.Localization.MultiLanguages
         public static IServiceCollection AddLanguageContainer(this IServiceCollection services, Assembly assembly, string folderName = "Resources")
         {
             services.AddSingleton<IKeysProvider, EmbeddedResourceKeysProvider>(s => new EmbeddedResourceKeysProvider(assembly, folderName));
-            return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            return services.AddSingleton<ILanguageContainerService, LanguageContainerInAssembly>(s =>
             {
                 var keysProvider = s.GetService<IKeysProvider>();
                 return new LanguageContainerInAssembly(keysProvider);
@@ -52,7 +52,7 @@ namespace AKSoftware.Localization.MultiLanguages
         where TKeysProvider : KeysProvider
         {
             services.AddSingleton<IKeysProvider, TKeysProvider>(s => (TKeysProvider)Activator.CreateInstance(typeof(TKeysProvider), assembly, folderName));
-            return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            return services.AddSingleton<ILanguageContainerService, LanguageContainerInAssembly>(s =>
             {
                 var keysProvider = s.GetService<IKeysProvider>();
                 return new LanguageContainerInAssembly(keysProvider);
@@ -63,11 +63,89 @@ namespace AKSoftware.Localization.MultiLanguages
             where TKeysProvider : KeysProvider
         {
             services.AddSingleton<IKeysProvider, TKeysProvider>(s => (TKeysProvider)Activator.CreateInstance(typeof(TKeysProvider), assembly, folderName, localizationFolderType));
+            return services.AddSingleton<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            {
+                var keysProvider = s.GetService<IKeysProvider>();
+                return new LanguageContainerInAssembly(keysProvider);
+            });
+        }
+
+
+        #region Blazor Server 
+        /// <summary>
+        /// Register a scoped instance of LanguageContainer class initialized with a specific culture
+        /// Scoped instance helps the Blazor Server to serve many clients with different languages at the same time
+        /// </summary>
+        /// <param name="services">Dependency Services provider</param>
+        /// <param name="assembly"></param>
+        /// <param name="culture">Initial culture</param>
+        /// <returns></returns>
+        public static IServiceCollection AddLanguageContainerForBlazorServer(this IServiceCollection services, Assembly assembly, CultureInfo culture, string folderName = "Resources")
+        {
+            services.AddSingleton<IKeysProvider, EmbeddedResourceKeysProvider>(s =>
+                new EmbeddedResourceKeysProvider(assembly, folderName));
+            return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            {
+                var keysProvider = s.GetService<IKeysProvider>();
+                return new LanguageContainerInAssembly(culture, keysProvider);
+            });
+        }
+
+        /// <summary>
+        /// Register a scoped instance of LanguageContainer class initialized with a specific culture
+        /// Scoped instance helps the Blazor Server to serve many clients with different languages at the same time
+        /// </summary>
+        /// <param name="services">Dependency Services provider</param>
+        /// <param name="assembly">Assembly that contains the Resource folder which has the language files</param>
+        /// <returns></returns>
+        public static IServiceCollection AddLanguageContainerForBlazorServer(this IServiceCollection services, Assembly assembly, string folderName = "Resources")
+        {
+            services.AddSingleton<IKeysProvider, EmbeddedResourceKeysProvider>(s => new EmbeddedResourceKeysProvider(assembly, folderName));
             return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
             {
                 var keysProvider = s.GetService<IKeysProvider>();
                 return new LanguageContainerInAssembly(keysProvider);
             });
         }
+
+        /// <summary>
+        /// Register a scoped instance of LanguageContainer class initialized with a specific culture
+        /// Scoped instance helps the Blazor Server to serve many clients with different languages at the same time
+        /// </summary>
+        /// <param name="services">Dependency Services provider</param>
+        /// <param name="assembly">Assembly that contains the Resource folder which has the language files</param>
+        /// <param name="folderName"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddLanguageContainerForBlazorServer<TKeysProvider>(this IServiceCollection services, Assembly assembly, string folderName = "Resources")
+        where TKeysProvider : KeysProvider
+        {
+            services.AddSingleton<IKeysProvider, TKeysProvider>(s => (TKeysProvider)Activator.CreateInstance(typeof(TKeysProvider), assembly, folderName));
+            return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            {
+                var keysProvider = s.GetService<IKeysProvider>();
+                return new LanguageContainerInAssembly(keysProvider);
+            });
+        }
+
+        /// <summary>
+        /// Register a scoped instance of LanguageContainer class initialized with a specific culture
+        /// Scoped instance helps the Blazor Server to serve many clients with different languages at the same time
+        /// </summary>
+        /// <param name="services">Dependency Services provider</param>
+        /// <param name="assembly">Assembly that contains the Resource folder which has the language files</param>
+        /// <param name="localizationFolderType">Define the type of the localization folder</param>
+        /// <param name="folderName"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddLanguageContainerForBlazorServer<TKeysProvider>(this IServiceCollection services, Assembly assembly, LocalizationFolderType localizationFolderType = LocalizationFolderType.InstallationFolder, string folderName = "Resources")
+            where TKeysProvider : KeysProvider
+        {
+            services.AddSingleton<IKeysProvider, TKeysProvider>(s => (TKeysProvider)Activator.CreateInstance(typeof(TKeysProvider), assembly, folderName, localizationFolderType));
+            return services.AddScoped<ILanguageContainerService, LanguageContainerInAssembly>(s =>
+            {
+                var keysProvider = s.GetService<IKeysProvider>();
+                return new LanguageContainerInAssembly(keysProvider);
+            });
+        }
+        #endregion 
     }
 }
