cf611025032d46af267c2274eac9607a5a5bb67e
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-10-05 16:33:23 +0400

    Create a benchmark project

---
+++ src/AKSoftware.Localization.MultiLanguages.Benchmarks/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AKSoftware.Localization.MultiLanguages;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Globalization;
+using System.Reflection;
+
+Console.WriteLine("Benchmark results of retrieving the data with JSON still being used");
+var summary = BenchmarkRunner.Run<LanguageContainerServiceBenchmarks>();
+Console.WriteLine(summary.Table.ToString());
+Console.ReadKey();
+
+public class LanguageContainerServiceBenchmarks
+{
+    ILanguageContainerService _service;
+
+    public LanguageContainerServiceBenchmarks()
+    {
+        var keysProvider = new EmbeddedResourceKeysProvider(Assembly.GetExecutingAssembly());
+        _service = new LanguageContainer(CultureInfo.GetCultureInfo("en-US"), keysProvider);
+    }
+
+    [Benchmark]
+    public string GetFirstLevelKey()
+    {
+        return _service.Keys["FooterMessage "];
+    }
+
+    [Benchmark]
+    public string GetLevelTwoKey()
+    {
+        return _service.Keys["HomePage:Login:Title"];
+    }
+}
