f8e40dc910773d8b30557ca22d6e98c4678faf28
J. Kwon <henry.kwon.0408@gmail.com>
2023-11-03 15:40:40 +0900

    create WPF sample app

---
+++ src/WpfSampleApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AKSoftware.Localization.MultiLanguages.Providers;
+using AKSoftware.Localization.MultiLanguages.WPF;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSampleApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App: Application
+    {
+        public static readonly WpfLanguageContainer LanguageContainer
+             = new(new("en-us"), new FolderResourceKeysProvider("Resources"));
+    }
+}
